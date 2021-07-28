26e228c8b552d2939081feeb3dfaac187e4bf38b
karbka <n.chernukhina@gmail.com>
2021-07-28 10:48:45 +0100

    Project for forms with flask -- added an owner functionality for a puppy adoption site

--- all_samples/05-Databases/03-Databases-in-Views/adoption_site.py
+++ all_samples/05-Databases/03-Databases-in-Views/adoption_site.py
@@ -1,8 +1,10 @@
 import os
-from forms import  AddForm , DelForm
+from forms import AddForm, DelForm, AddOwnerForm
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+
+
 app = Flask(__name__)
 # Key for Forms
 app.config['SECRET_KEY'] = 'mysecretkey'
@@ -19,17 +21,38 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 Migrate(app,db)
 
+
 class Puppy(db.Model):
 
     __tablename__ = 'puppies'
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.Text)
 
+    # one-to-one
+    owner = db.relationship('Owner', backref='puppy', uselist=False)
+
     def __init__(self,name):
         self.name = name
 
     def __repr__(self):
-        return f"Puppy name: {self.name}"
+        if self.owner:
+            return f"Puppy name: {self.name}, owner: {self.owner.name}"
+        else:
+            return f"Puppy name: {self.name}, no owner"
+
+
+class Owner(db.Model):
+
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    puppy_id = db.Column(db.Integer, db.ForeignKey('puppies.id'))
+
+    def __init__(self, name, puppy_id):
+        self.name = name
+        self.puppy_id = puppy_id
+
 
 ############################################
 
@@ -39,6 +62,7 @@ class Puppy(db.Model):
 @app.route('/')
 def index():
     return render_template('home.html')
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_pup():
@@ -56,11 +80,13 @@ def add_pup():
 
     return render_template('add.html',form=form)
 
+
 @app.route('/list')
 def list_pup():
     # Grab a list of puppies from database.
     puppies = Puppy.query.all()
     return render_template('list.html', puppies=puppies)
+
 
 @app.route('/delete', methods=['GET', 'POST'])
 def del_pup():
@@ -77,5 +103,23 @@ def del_pup():
     return render_template('delete.html',form=form)
 
 
+@app.route('/add_owner', methods=['GET', 'POST'])
+def add_owner():
+    form = AddOwnerForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        puppy_id = form.puppy_id.data
+
+        # Add new owner to database
+        new_owner = Owner(name, puppy_id)
+        db.session.add(new_owner)
+        db.session.commit()
+
+        return redirect(url_for('list_pup'))
+
+    return render_template('add_owner.html',form=form)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5010)
--- all_samples/05-Databases/03-Databases-in-Views/forms.py
+++ all_samples/05-Databases/03-Databases-in-Views/forms.py
@@ -2,13 +2,20 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
 
 
-
 class AddForm(FlaskForm):
 
     name = StringField('Name of Puppy:')
     submit = SubmitField('Add Puppy')
 
+
 class DelForm(FlaskForm):
 
     id = IntegerField('Id Number of Puppy to Remove:')
     submit = SubmitField('Remove Puppy')
+
+
+class AddOwnerForm(FlaskForm):
+
+    name = StringField('Owner Name:')
+    puppy_id = IntegerField('Puppy id: ')
+    submit = SubmitField('Add Owner')
